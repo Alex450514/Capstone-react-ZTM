2c1f9535e8ed93ce4da146bd45c53fa2f42aa2d4
Alexandre Cusson <48602248+Alex450514@users.noreply.github.com>
2024-03-26 23:18:42 -0400

    added redux-persist

--- crown-clothing/src/index.js
+++ crown-clothing/src/index.js
@@ -7,17 +7,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { CartProvider } from './contexts/cart.context';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
+
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-          <CartProvider>
-            <App />
-          </CartProvider>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+            <CartProvider>
+              <App />
+            </CartProvider>
+      </BrowserRouter>
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- crown-clothing/src/store/store.js
+++ crown-clothing/src/store/store.js
@@ -3,11 +3,25 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import rootReducer from "./root-reducer";
 
+import { persistStore, persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['categories'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
       process.env.NODE_ENV !== 'production'
         ? getDefaultMiddleware().concat(logger)
         : getDefaultMiddleware(),
   });
+
+export const persistor = persistStore(store);
 
